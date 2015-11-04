512e6199f81602a7c9d0bf3ecdcfca5388b9a4e8
fanruiyang <386031585@qq.com>
2015-11-04 20:16:32 +0800

    雪花

---
+++ 雪花飘飘/雪花飘飘/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  雪花飘飘
+//
+//  Created by qingyun on 15/11/4.
+//  Copyright (c) 2015年 qingyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 雪花飘飘/雪花飘飘/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  雪花飘飘
+//
+//  Created by qingyun on 15/11/4.
+//  Copyright (c) 2015年 qingyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
